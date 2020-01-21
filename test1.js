9185d6d896cdaffde4c7e1e94558a1637071e46a
Russell Wright <russell.bwright@yahoo.com>
2020-01-21 01:27:53 -0600

    Jaden_Case_7

---
+++ test1.js
@@ -0,0 +1,14 @@
+// you get an array of numbers, 
+// return the sum of all of the positives ones.
+// EX [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// function positiveSum(arr){
+//     var total = 0;
+//     for (i = 0; i < arr.length; i++){
+//         if(arr[i] > 0){
+//             total += arr[i];
+//         }
+//     }
+//     return total
+// }
+
